b2f1b0ce4183c5909ac55d2a9b1caa1216dabb8c
qamaldeen-usman <qamaldeenusman@gmail.com>
2026-06-29 22:25:30 +0100

    Fixed cloud functions to use dynamic imports and added missing deps

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,20 @@
 const functions = require('firebase-functions');
-// Import the reqHandler from your built server file
-const { reqHandler } = require('../dist/gadgettemplate/server/server.mjs');
 
-// Export the function named 'ssr'
-exports.ssr = functions.https.onRequest(reqHandler);
+// We'll keep the loaded app in a variable
+let ssrApp = null;
+
+exports.ssr = functions.https.onRequest(async (req, res) => {
+  // If the app hasn't loaded yet, load it dynamically (this handles .mjs correctly)
+  if (!ssrApp) {
+    try {
+      const { reqHandler } = await import('../dist/gadgettemplate/server/server.mjs');
+      ssrApp = reqHandler;
+    } catch (err) {
+      console.error('Failed to load SSR server:', err);
+      res.status(500).send('SSR Server error');
+      return;
+    }
+  }
+  // Run the request handler
+  ssrApp(req, res);
+});
