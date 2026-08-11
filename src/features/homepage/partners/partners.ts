d1152e8633b19bb488a5ce45b9d82ba26cee1466
qamaldeen-usman <qamaldeenusman@gmail.com>
2026-08-11 16:11:41 +0100

    fixed swiper document not defined in partners dir by using platform_id

--- src/features/homepage/partners/partners.ts
+++ src/features/homepage/partners/partners.ts
@@ -1,5 +1,5 @@
-import { Component, ChangeDetectionStrategy, AfterViewInit } from '@angular/core';
-// import { NgOptimizedImage } from '@angular/common';
+import { Component, ChangeDetectionStrategy, AfterViewInit, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Autoplay, FreeMode } from 'swiper/modules';
 import { partnersList } from './partners-list';
 import Swiper from 'swiper';
@@ -13,6 +13,7 @@ import { NgOptimizedImage } from '@angular/common';
   styleUrl: './partners.scss',
 })
 export class Partners implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
   protected readonly partnersList = partnersList;
 
   get reversedPartnersList() {
@@ -20,52 +21,56 @@ export class Partners implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    //small timeout to ensure DOM is fully rendered
-    setTimeout(() => {
-      //First swiper - using the class we'll add to template
-      new Swiper('.partners-swiper-normal', {
-        modules: [Autoplay, FreeMode],
-        loop: true,
-        grabCursor: false,
-        centeredSlides: true,
-        autoplay: {
-          delay: 0,
-          disableOnInteraction: false,
-        },
-        freeMode: true,
-        speed: 10000,
-        breakpoints: {
-          0: { slidesPerView: 2 },
-          600: { slidesPerView: 3 },
-          800: { slidesPerView: 4 },
-          1200: { slidesPerView: 5 },
-          1400: { slidesPerView: 6 },
-          1600: { slidesPerView: 7 },
-        },
-      });
 
-      //Second swiper (RTL)
-      new Swiper('.partners-swiper-rtl', {
-        modules: [Autoplay, FreeMode],
-        direction: 'horizontal',
-        loop: true,
-        grabCursor: false,
-        centeredSlides: true,
-        autoplay: {
-          delay: 0,
-          disableOnInteraction: false,
-        },
-        freeMode: true,
-        speed: 8500,
-        breakpoints: {
-          0: { slidesPerView: 2 },
-          600: { slidesPerView: 3 },
-          800: { slidesPerView: 4 },
-          1200: { slidesPerView: 5 },
-          1400: { slidesPerView: 6 },
-          1600: { slidesPerView: 7 },
-        },
-      });
-    }, 100);
-  }
+    if (isPlatformBrowser(this.platformId)) {
+      //small timeout to ensure DOM is fully rendered
+      setTimeout(() => {
+        //First swiper - using the class we'll add to template
+        new Swiper('.partners-swiper-normal', {
+          modules: [Autoplay, FreeMode],
+          loop: true,
+          grabCursor: false,
+          centeredSlides: true,
+          autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+          },
+          freeMode: true,
+          speed: 10000,
+          breakpoints: {
+            0: { slidesPerView: 2 },
+            600: { slidesPerView: 3 },
+            800: { slidesPerView: 4 },
+            1200: { slidesPerView: 5 },
+            1400: { slidesPerView: 6 },
+            1600: { slidesPerView: 7 },
+          },
+        });
+
+        //Second swiper (RTL)
+        new Swiper('.partners-swiper-rtl', {
+          modules: [Autoplay, FreeMode],
+          direction: 'horizontal',
+          loop: true,
+          grabCursor: false,
+          centeredSlides: true,
+          autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+          },
+          freeMode: true,
+          speed: 8500,
+          breakpoints: {
+            0: { slidesPerView: 2 },
+            600: { slidesPerView: 3 },
+            800: { slidesPerView: 4 },
+            1200: { slidesPerView: 5 },
+            1400: { slidesPerView: 6 },
+            1600: { slidesPerView: 7 },
+          },
+        });
+      }, 100);
+    }
+    }
+
 }
