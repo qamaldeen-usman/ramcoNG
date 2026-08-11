854c5a8f7d1e9e722b4da23b3bb03518ec26369e
qamaldeen-usman <qamaldeenusman@gmail.com>
2026-08-11 19:34:59 +0100

    used sqoosh to reduce the size of swiper img and updated their wandh props in partner.ts

--- src/features/homepage/partners/partners-list.ts
+++ src/features/homepage/partners/partners-list.ts
@@ -1,12 +1,12 @@
 export const partnersList = [
   {
     name: 'Shello Computers',
-    logo: 'assets/ramcobgs/shello-computers-logo-web.png',
-    width: 1536,
-    height: 1024,
+    logo: 'assets/ramcobgs/shello-computers-logo-web (1).webp',
+    width: 400,
+    height: 267,
   },
   { name: 'Kogi Government', logo: 'assets/ramcobgs/kogigovern.png', width: 150, height: 49 },
-  { name: 'FUL', logo: 'assets/ramcobgs/logo.png', width: 1635, height: 366 },
+  { name: 'FUL', logo: 'assets/ramcobgs/logo.webp', width: 500, height: 112 },
   { name: 'Kogi Polytechnic', logo: 'assets/ramcobgs/ksp.jpg', width: 246, height: 77 },
   { name: 'Osara University', logo: 'assets/ramcobgs/osara.jpg', width: 576, height: 318 }, // Est. if you don't know, check file
   { name: 'PAUUN', logo: 'assets/ramcobgs/pauun.png', width: 300, height: 210 }, // Est.
